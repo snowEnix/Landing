a6db0464fce447cebbb9c0828bbe3f3ac2f0f951
snowEnix <alexey.svidrov@gmail.com>
2022-04-16 18:46:46 +0300

    burger

---
+++ scripts.js
@@ -0,0 +1,21 @@
+dropDown = () => {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = (event) => {
+  if (!event.target.matches('.menu__item-link')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+burger = () => {
+  document.getElementById ("burger").classList.toggle("active");
+}
